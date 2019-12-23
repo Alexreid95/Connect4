025a9bf025066828eb50a92d3495edcc7ec1178d
Alex Reid <42113044+Alexreid95@users.noreply.github.com>
2019-12-23 22:12:24 +0000

    Update game.js
    fixed error

--- game.js
+++ game.js
@@ -2,12 +2,10 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  backgroundColor: 0x43676B,
-  scene: {
-    gameStart,
-    gamePlay,
-    gameOver
-  }
+  backgroundColor: "#FFEE6D",
+  scene: [
+    GameStart
+  ]
 };
 
 var game = new Phaser.Game(config);
